810e5b5854a50f177c0880deb2c3bd4ceaeddad3
Marion-gillot <marion.gillot@edu.univ-fcomte.fr>
2022-06-01 16:17:51 +0200

    coordonées de Firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router'
 import './index.css'
 
 const app = createApp(App)
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB4eSJog62GJdnK_u8yhtR6fT61nNiv21o",
+    authDomain: "sae203-marion-gillot.firebaseapp.com",
+    projectId: "sae203-marion-gillot",
+    storageBucket: "sae203-marion-gillot.appspot.com",
+    messagingSenderId: "827131448601",
+    appId: "1:827131448601:web:d6d56cdd2511887012c988"
+  };
 
 app.use(router)
 
