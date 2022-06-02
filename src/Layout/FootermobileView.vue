048a557d32a64298b9fac9d2ae072c5cd32617da
Marion-gillot <marion.gillot@edu.univ-fcomte.fr>
2022-06-02 10:51:06 +0200

    footer responsive

--- src/Layout/FootermobileView.vue
+++ src/Layout/FootermobileView.vue
@@ -4,6 +4,25 @@
              <RouterLink to="/"><img src="/img/logosunset.png" class="w-12 h-12 mx-7 my-5 " /></RouterLink>
         </div>
 
+        <div class="mx-7 mb-6">
+            <p class="mb-3 font-bold">Festival d'Arcachon</p>
+            <p class="mb-3">Adresse de la Plage centrale : Boulevard Promenade Veyrier Montagnères 33120 Arcachon</p>
+            <p class="mb-3">00 00 00 00 00</p>
+        </div>
+
+        <div class="flex gap-6 mx-7">
+            <div><a href="https://www.facebook.com/profile.php?id=100009306961064" aria-label="Facebook">
+              <img src="/icon/facebook.png" alt="icon facebook"></a></div>
+            <div><a href="https://www.linkedin.com/in/marion-gillot-8133a7224/" aria-label="Instagram">
+              <img src="/icon/instagram.png" alt="icon instagram"></a></div>
+            <div><a href="https://twitter.com/1820Tc" aria-label="Twitter">
+              <img src="/icon/twitter.png" alt="icon twitter"></a></div>
+            <div class="w-6"><a href="https://www.linkedin.com/in/marion-gillot-8133a7224/" aria-label="Linlkedin">
+              <img src="/icon/mmi.svg" alt="icon mmi"></a></div>
+        </div>
+        
+        <p class="mt-10 text-center">©Sunset</p>
+
     
 
     </div>
