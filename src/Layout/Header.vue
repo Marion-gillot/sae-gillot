ce53ebea9fb31b2d854ccb1eb81488b21ca1258b
Marion-gillot <marion.gillot@edu.univ-fcomte.fr>
2022-06-03 17:33:56 +0200

    correction lien logo sunset

--- src/Layout/Header.vue
+++ src/Layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="z-50 flex h-16 w-screen ">
-     <img src="img/logosunset.png" class="h-12 mx-7 my-5 w-auto mt-5" />
+     <img src="/img/logosunset.png" class="h-12 mx-7 my-5 w-auto mt-5" />
     <MenuView class="ml-auto z-50 h-8 w-8 mt-5 right-5" @click="MenuOpen = !MenuOpen" v-if="!MenuOpen" />
     <XView class="ml-auto z-50 h-8 w-8 mt-5 right-5" @click="MenuOpen = !MenuOpen" v-if="MenuOpen" />
     <nav class="absolute left-0 top-0 bottom-0 right-0 h-screen -translate-x-full  bg-gradient-to-r from-degrade-1 via-degrade-2 to-degrade-3  opacity-0" :class="MenuOpen && 'translate-x-0 opacity-100'">
@@ -29,7 +29,6 @@
 </template>
 
 <script>
-import LogoHeader from "/img/logosunset.png";
 import MenuView from "../component/MenuView.vue";
 import XView from "../component/XView.vue";
 
@@ -39,6 +38,6 @@ export default {
       MenuOpen: false,
     };
   },
-  components: { LogoHeader, MenuView, XView },
+  components: { MenuView, XView },
 };
 </script> 
